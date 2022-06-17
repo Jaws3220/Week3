3af1235ccac418f541c28896cabdcff009008408
jaws3220 <jaws32207@gmail.com>
2022-06-17 13:07:27 -0700

    completed part of Lab upload

--- ArraysAndFunctions.js
+++ ArraysAndFunctions.js
@@ -2,27 +2,52 @@ let array1 = [1, 5, 6, 9, 10, 14];
 
 // print the 3rd element in array1
 
+console.log (array1 [2])
+
 // print the last element in array1
 
+console.log(array1 [5])
+
 // add 16 and 3 to array1
+
+array1.push(16, 3)
 
 // sort the array, then print the 3rd element again
 // did it change?
 
+array1.sort()
+console.log(array1[2])
+//after the sort the new element in the 3rd position instead of the 6
 
 // create a variable called myTodoList that holds an empty array
 
+let myTodoList = []
+
 // add three todo items to the array using a built-in array method
+
+myTodoList.push('sort')
+myTodoList.push('clean')
+myTodoList.push('wash')
 
 // remove the second item in the array
 
+myTodoList.splice(1)
+
 // create another array, yourTodoList, and add two todo items
 
+let yourTodoList = ['cook', 'study']
+
 // create another array, ourTodoList
+
+let ourTodoList = []
+
 // combine myTodoList and yourTodoList into ourTodoList
 
+ourTodoList = myTodoList.concat(yourTodoList)
 
 // sort the following array from Z-A
+
+ourTodoList.sort()
 
 // create a function called reverse that takes in parameter
 // this function will return the opposite of whatever is passed in
